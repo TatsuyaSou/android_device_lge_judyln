447c9a5c08bd407e9fb41f656af1dce1a7803858
Radosław <65538949+TatsuyaSou@users.noreply.github.com>
2021-09-21 18:08:08 +0200

    Hello DotOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -16,8 +16,8 @@
 #
 
 PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/lineage_judyp.mk
+    $(LOCAL_DIR)/dot_judyp.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_judyp-userdebug \
-    lineage_judyp-eng
+    dot_judyp-userdebug \
+    dot_judyp-eng
